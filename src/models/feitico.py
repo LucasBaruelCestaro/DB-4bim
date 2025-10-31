61f6ef1cca6306095d0f597b766535e51428b4ca
Lucas Baruel Cestaro <lucasbaruel9@gmail.com>
2025-10-30 22:22:40 -0300

    classe Feitico finalizada

--- src/models/feitico.py
+++ src/models/feitico.py
@@ -33,6 +33,15 @@ class Feitico:
     
     @difficulty.setter
     def difficulty(self, value: int) -> None:
+        if not isinstance(value, int):
+            raise TypeError("A dificuldade do feitiço deve ser um número inteiro.")
+        
+        if value < 1 or value > 10:
+            raise ValueError("A dificuldade do feitiço deve estar entre 1 e 10.")
+        
+        if not value:
+            raise ValueError("A dificuldade do feitiço não pode ser nula.")
+
         self._difficulty = value
 
     @property
@@ -41,6 +50,15 @@ class Feitico:
     
     @ingredients.setter
     def ingredients(self, value: List[str]) -> None:
+        if not isinstance(value, list) or not all(isinstance(i, str) for i in value):
+            raise TypeError("Os ingredientes devem ser uma lista de strings.")
+        
+        if not value:
+            raise ValueError("A lista de ingredientes não pode estar vazia.")
+        
+        if any(len(i.strip()) == 0 for i in value):
+            raise ValueError("Os ingredientes não podem conter strings vazias.")
+
         self._ingredients = value
 
     @property
@@ -49,6 +67,15 @@ class Feitico:
     
     @description.setter
     def description(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise TypeError("A descrição do feitiço deve ser uma string.")
+        
+        if not value:
+            raise ValueError("A descrição do feitiço não pode ser vazia.")
+        
+        if len(value.strip()) < 5:
+            raise ValueError("A descrição do feitiço deve ter pelo menos 5 caracteres.")
+
         self._description = value
 
     @property
@@ -57,6 +84,12 @@ class Feitico:
     
     @casting_time.setter
     def casting_time(self, value: Annotated[int, "Em segundos"]) -> None:
+        if not isinstance(value, int):
+            raise TypeError("O tempo de conjuração deve ser um número inteiro.")
+        
+        if value < 0:
+            raise ValueError("O tempo de conjuração não pode ser negativo.")
+
         self._casting_time = value
 
     def to_dict(self) -> dict:
