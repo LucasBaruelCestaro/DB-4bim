308a820d908d90a7f6beec5a51d2bb88909d40c7
Lucas Baruel Cestaro <lucasbaruel9@gmail.com>
2025-11-02 15:36:23 -0300

    create() do feiticeiroDAO

--- src/data/feiticeiroDAO.py
+++ src/data/feiticeiroDAO.py
@@ -3,9 +3,9 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from bson import ObjectId
 from db.database import Database
 from models.feiticeiro import Feiticeiro
+
 
 class FeiticeiroDAO:
     COLLECTION_NAME = "feiticeiros"
@@ -13,52 +13,25 @@ class FeiticeiroDAO:
     def __init__(self):
         self.collection = Database.get_collection(self.COLLECTION_NAME)
 
-    def create(self, feiticeiro: Feiticeiro) -> Feiticeiro:
+    def create(self, feiticeiro: Feiticeiro) -> int:
+        if feiticeiro.intID is not None:
+            pass
+        else:
+            feiticeiro.intID = self._get_next_id()
+
         data = feiticeiro.to_dict()
         result = self.collection.insert_one(data)
-        feiticeiro.id = str(result.inserted_id)
-        return feiticeiro
 
-    def read_all(self) -> list[Feiticeiro]:
-        feiticeiros_data = list(self.collection.find())
-        feiticeiros = []
-        for data in feiticeiros_data:
-            data["id"] = str(data["_id"])
-            data.pop("_id", None)
-            feiticeiros.append(Feiticeiro.from_dict(data))
-        return feiticeiros
+        return result.inserted_id
 
-    def read_by_id(self, feiticeiro_id: str) -> Feiticeiro | None:
-        try:
-            object_id = ObjectId(feiticeiro_id)
-        except Exception:
-            return None
-
-        data = self.collection.find_one({"_id": object_id})
-        if not data:
-            return None
-
-        data["id"] = str(data["_id"])
-        data.pop("_id", None)
-        return Feiticeiro.from_dict(data)
-
-    def update(self, feiticeiro_id: str, feiticeiro: Feiticeiro) -> bool:
-        try:
-            object_id = ObjectId(feiticeiro_id)
-        except Exception:
-            return False
-
-        result = self.collection.update_one(
-            {"_id": object_id},
-            {"$set": feiticeiro.to_dict()}
+    def _get_next_id(self) -> int:
+        last_feiticeiro = self.collection.find_one(
+            sort=[("intID", -1)]
         )
-        return result.modified_count > 0
-
-    def delete(self, feiticeiro_id: str) -> bool:
-        try:
-            object_id = ObjectId(feiticeiro_id)
-        except Exception:
-            return False
-
-        result = self.collection.delete_one({"_id": object_id})
-        return result.deleted_count > 0
+        if last_feiticeiro and "intID" in last_feiticeiro:
+            return last_feiticeiro["intID"] + 1
+        else:
+            return 1
+        
+if __name__ == "__main__":
+    pass
